8e46de1f832efbddb48160bc4396369ec38ec494
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-17 19:35:43 +0530

    <Init Project:*/*>?

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+      <>
+          <div className="page">
+              <div className="h-screen w-screen flex items-center justify-center">
+                  <div className="flex flex-col items-center justify-center">
+                      {/*overflow-x-auto text-center*/}
+                      <h1 className="text-6xl font-bold ps-1 pe-1 items-center justify-center text-center">Welcome to Fitbit</h1>
+                        <div className="mt-5"></div>
+                      <h1 className="text-gray-400 text-3xl font-bold ">UI Soon </h1>
+                  </div>
+              </div>
+          </div>
+      </>
+  );
+}
