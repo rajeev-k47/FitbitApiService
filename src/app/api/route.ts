68610013dfb56fd8b73c2ae3bbce568d8ee6e826
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-18 12:12:49 +0530

    <Init,New:Group request notifications>?

---
+++ src/app/api/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import { firestore, messaging } from '@/app/lib/firebaseAdmin';
+
+export async function POST(request: Request) {
+    try {
+        const { uid, groupId } = await request.json();
+
+        if (!uid || !groupId) {
+            return NextResponse.json({ error: 'UID and Group ID are required' }, { status: 400 });
+        }
+        const groupDoc = await firestore.collection('users').doc(groupId).get();
+        if (!groupDoc.exists) {
+            return NextResponse.json({ error: 'Group not found' }, { status: 404 });
+        }
+        const userDoc = await firestore.collection('users').doc(uid).get();
+        if (!userDoc.exists) {
+            return NextResponse.json({ error: 'User not found' },{ status: 404 });
+        }
+
+        const groupData = groupDoc.data();
+
+        const fcmToken = groupData?.fcmToken;
+
+        if (!fcmToken) {
+            return NextResponse.json({ error: 'Group owner FCM token not found' }, { status: 404 });
+        }
+        const message = {
+            token: fcmToken,
+            notification: {
+                title: 'New Joining Request',
+                body: `You have a new joining request pending from  ${groupId}.`,
+            },
+            data: {
+                groupId: groupId,
+            },
+        };
+        await messaging.send(message);
+
+        return NextResponse.json({ message: 'Notification sent to group owner' });
+
+    } catch (error) {
+        console.error('Error sending notification:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
