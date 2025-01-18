2f06a9869693ec308f1a349a64ad92fbcefeefda
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-18 12:12:22 +0530

    <UI:*/*>?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
                       {/*overflow-x-auto text-center*/}
                       <h1 className="text-6xl font-bold ps-1 pe-1 items-center justify-center text-center">Welcome to Fitbit</h1>
                         <div className="mt-5"></div>
+
                       <h1 className="text-gray-400 text-3xl font-bold ">UI Soon </h1>
+                      {
+
+                      }
                   </div>
               </div>
           </div>
